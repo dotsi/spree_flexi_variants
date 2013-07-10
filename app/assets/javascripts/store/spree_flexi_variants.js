7babb7d1f4ced4b1b8d6a580cdc1ff8e3962a274
Miha <miha.kersic@gmail.com>
2013-07-11 00:08:11 +0200

    ready for production

--- app/assets/javascripts/store/spree_flexi_variants.js
+++ app/assets/javascripts/store/spree_flexi_variants.js
@@ -1,5 +1,5 @@
-//= require jquery.formatCurrency-1.4.0.min
-//= require i18n/jquery.formatCurrency.all
+// require jquery.formatCurrency-1.4.0.min
+// require i18n/jquery.formatCurrency.all
 //= require_self
 
 $(function () {
